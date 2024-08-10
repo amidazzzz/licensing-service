fba1e3bdd6f8aa47d130b0b1e14be2e97e226b03
amida <cain.wells@mail.ru>
2024-08-10 16:03:55 +0300

    Initial commit

---
+++ src/test/java/org/amida/licensing_service/LicenseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amida.licensing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
