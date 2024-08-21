87805110cb91ce2dcb820a47866cdab0ab74f856
amida <cain.wells@mail.ru>
2024-08-21 13:38:27 +0300

    Сonfigured the basic structure of the project

---
+++ src/main/java/org/amida/licensing_service/controller/LicenseController.java
@@ -0,0 +1,55 @@
+package org.amida.licensing_service.controller;
+
+import org.amida.licensing_service.model.License;
+import org.amida.licensing_service.service.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+
+@RestController
+@RequestMapping(value="v1/organization/{organizationId}/license")
+public class LicenseController {
+
+    private final LicenseService licenseService;
+
+    public LicenseController(LicenseService licenseService) {
+        this.licenseService = licenseService;
+    }
+
+    @GetMapping("/{licenseId}")
+    public ResponseEntity<License> getLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+
+        License license = licenseService.getLicense(licenseId, organizationId);
+        return ResponseEntity.ok(license);
+
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License request){
+
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License request, @RequestHeader(value = "Accept-Language",required = false)
+            Locale locale) {
+
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
+    }
+
+    @DeleteMapping(value="/{licenseId}")
+    public ResponseEntity<String> deleteLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId,
+                organizationId));
+    }
+}
---
+++ src/main/java/org/amida/licensing_service/service/LicenseService.java
@@ -0,0 +1,67 @@
+package org.amida.licensing_service.service;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import org.amida.licensing_service.model.License;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+import java.util.Random;
+
+@Service
+@Slf4j
+public class LicenseService {
+
+    @Autowired
+    MessageSource messages;
+
+    public License getLicense(String licenseId, String organizationId){
+        License license = new License();
+        license.setId(new Random().nextInt(1000));
+        license.setLicenseId(licenseId);
+        license.setOrganizationId(organizationId);
+        license.setDescription("Software product");
+        license.setProductName("Ostock");
+        license.setLicenseType("full");
+
+        log.info("Created License with Id: {}", license.getId());
+
+
+        return license;
+    }
+
+    public String updateLicense(License license, String organizationId){
+        String responseMessage = null;
+        if (license != null) {
+            license.setOrganizationId(organizationId);
+            responseMessage = String.format(messages.getMessage(
+                            "license.update.message", null, null),
+                    license.toString());
+        }
+
+        return responseMessage;
+    }
+
+    public String createLicense(License license, String organizationId, Locale locale){
+        String responseMessage = null;
+        if (license != null) {
+            license.setOrganizationId(organizationId);
+            responseMessage = String.format(messages.getMessage(
+                            "license.create.message", null,locale),
+                    license.toString());
+        }
+
+        return responseMessage;
+    }
+
+    public String deleteLicense(String licenseId, String organizationId) {
+        String responseMessage = null;
+        responseMessage = String.format(
+                "Deleting license with id %s for the organization %s",
+                licenseId, organizationId);
+
+        return responseMessage;
+    }
+}
